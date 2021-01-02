a137991b78a6a24ca2457f86aa55c513ee7ed5da
KimJoongKi <thoug12@naver.com>
2021-01-02 18:06:01 +0900

    Initial commit

---
+++ src/main/java/yaddoong/feemanage/FeeManageApplication.java
@@ -0,0 +1,13 @@
+package yaddoong.feemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeeManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeeManageApplication.class, args);
+    }
+
+}
