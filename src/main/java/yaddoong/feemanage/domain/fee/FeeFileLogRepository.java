95a600bac94550cc960a01f937bb35c40e1fedc0
KimJoongKi <thoug12@naver.com>
2021-01-25 15:54:51 +0900

    파일 업로드 데이터 조회 추가

--- src/main/java/yaddoong/feemanage/domain/fee/FeeFileLogRepository.java
+++ src/main/java/yaddoong/feemanage/domain/fee/FeeFileLogRepository.java
@@ -1,6 +1,8 @@
 package yaddoong.feemanage.domain.fee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface FeeFileLogRepository extends JpaRepository<FeeFileLog, Long> {
+public interface FeeFileLogRepository extends PagingAndSortingRepository<FeeFileLog, Long> {
 }
--- src/main/java/yaddoong/feemanage/web/FeeController.java
+++ src/main/java/yaddoong/feemanage/web/FeeController.java
@@ -1,13 +1,18 @@
 package yaddoong.feemanage.web;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import yaddoong.feemanage.domain.fee.FeeFileLog;
+import yaddoong.feemanage.domain.fee.FeeFileLogRepository;
 import yaddoong.feemanage.domain.fee.FeeLog;
 import yaddoong.feemanage.service.fee.FeeService;
-import yaddoong.feemanage.web.form.FeeFileForm;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,6 +25,7 @@ import java.util.List;
 public class FeeController {
 
     private final FeeService feeService;
+    private final FeeFileLogRepository feeFileLogRepository;
 
     @GetMapping(value = "/new")
     public String saveForm() {
@@ -27,7 +33,18 @@ public class FeeController {
     }
 
     @GetMapping(value = "/list")
-    public String list() {
+    public String list(Model model) {
+        Pageable pageable = PageRequest.of(0, 1, Sort.by(Sort.Direction.DESC, "name"));
+        Page<FeeFileLog> all = feeFileLogRepository.findAll(pageable);
+        if (all.getContent().size() == 0) {
+            model.addAttribute("lastUpdateData", "업데이트 기록 없음");
+            return "fee/list";
+        }
+        model.addAttribute
+                ("lastUpdateData",all.getContent()
+                        .get(0)
+                        .getName()
+                        .replaceAll(".xlsx","") + " 기준");
         //미납금 + (오늘 날짜 - 오늘 날짜가 15보다 적으면 저번 달 15일 or 이번달 15일)*
         return "fee/list";
     }
@@ -39,3 +56,4 @@ public class FeeController {
     }
 
 }
+
