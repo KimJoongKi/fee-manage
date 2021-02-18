d11ed61708805005abcda3f89ab6a6bffe0af952
KimJoongKi <thoug12@naver.com>
2021-02-18 09:01:08 +0900

    테스트 공부 중,,

---
+++ src/test/java/yaddoong/feemanage/JavaTest.java
@@ -0,0 +1,23 @@
+package yaddoong.feemanage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JavaTest {
+
+    @Test
+    public void 운영체제_확인() {
+        String property = System.getProperty("os.name").toUpperCase();
+        System.out.println("property = " + property);
+
+        
+        if(property.indexOf("WIN") >= 0) {
+            System.out.println("윈도우");
+            return;
+        }
+        System.out.println("노윈도우");
+
+        
+    }
+}
--- src/test/java/yaddoong/feemanage/service/fee/FeeServiceTest.java
+++ src/test/java/yaddoong/feemanage/service/fee/FeeServiceTest.java
@@ -1,5 +1,8 @@
 package yaddoong.feemanage.service.fee;
 
+import org.aspectj.lang.annotation.Before;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +11,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.io.*;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -15,6 +19,38 @@ class FeeServiceTest {
 
     @Autowired
     FeeService feeService;
+
+//    @BeforeAll
+    public void 사전작업() {
+
+        String property = System.getProperty("os.name");
+
+        System.out.println("property = " + property);
+
+        String originalFilePath = "C:\\tmp\\test\\2021년1월23일.xlsx";
+        String tmpPath = System.getProperty("user.dir") + "\\tmp";
+        String copyFilePath = tmpPath + "\\2021년1월23일.xlsx";
+
+        File originalFile = new File(originalFilePath);
+        File copyFile = new File(copyFilePath);
+
+        try {
+            FileInputStream fis = new FileInputStream(originalFile);
+            FileOutputStream fos = new FileOutputStream(copyFile);
+
+            int fileByte = 0;
+            while ((fileByte = fis.read()) != -1) {
+                fos.write(fileByte);
+            }
+            fis.close();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Test
     public void 서비스호출() {
@@ -46,6 +82,7 @@ class FeeServiceTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
 
     }
 
